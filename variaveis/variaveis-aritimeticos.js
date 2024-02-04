91033d73b404703b5f5a763e37c727f7a88b40e9
Thalita coelho <thalita.bastos94@gmail.com>
2024-02-03 21:57:39 -0300

    aprendendo um pouco de concatenacao infinity e NaN, e exercitando um pouco de variaveis aritimeticos.

--- variaveis/variaveis-aritimeticos.js
+++ variaveis/variaveis-aritimeticos.js
@@ -1,13 +1,14 @@
 let x = 10, y = 2;
 
 let resultado = x + y;
-console.log(resultado); //12
-
+let mensagem = "A soma de " + x + " e " + y + " é " + resultado; // jeito um pouco mais complicado 
+console.log(`A soma de ${x} e ${y} é ${resultado}`); //12
+// jeito mais facil
 resultado = x-y;
-console.log(resultado); //8
+console.log(`A subtração de ${x} e ${y} é ${resultado}`); //8
 
 resultado = x*y; //20
-console.log(resultado);
+console.log(mensagem);
 
 resultado = x/y; //5
-console.log(resultado);
+console.log(`A divisão de ${x} e ${y} é ${resultado}`);
---
+++ variaveis/variaveis-concatenacao.js
@@ -0,0 +1,5 @@
+let nome = "Thalita";
+let sobrenome = "Coelho";
+
+let nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
