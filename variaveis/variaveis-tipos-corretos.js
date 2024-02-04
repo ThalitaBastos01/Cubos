3b06dc3253a22567c3af40406b8b84ff8008a6c3
Thalita coelho <thalita.bastos94@gmail.com>
2024-02-03 22:12:08 -0300

    aprendendo o jeito certo dos tipos e a hora exata de quando ultilizar.

---
+++ variaveis/variaveis-tipos-corretos.js
@@ -0,0 +1,4 @@
+let x = "10", y = 2;
+
+let resultado = x + y;
+console.log(`A soma de ${x} e ${y} é ${resultado}`); // resultado da 102, mas deu errado, por que declaramos a variavel x com uma string então ela concatenou com o 2
