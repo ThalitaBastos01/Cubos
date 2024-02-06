3d140253d19281689bbe272d29d176272a3af215
Thalita coelho <thalita.bastos94@gmail.com>
2024-02-05 21:39:59 -0300

    operadores-comparacao.js

---
+++ condicionais/operadores-identidade.js
@@ -0,0 +1,2 @@
+const ehIdentico = 18 == 18; //=== alem dele analisar se é o mesmo conteudo, verifica tbm se é o mesmo tipo.
+console.log(ehIdentico);
---
+++ condicionais/operadores-igualdade.js
@@ -0,0 +1,3 @@
+let x = 10;
+const ehIgual = (x != 18); // 10 é diferente que 18.
+console.log(ehIgual); //o resultado da variavel const vai ser guardado e imprimido true
