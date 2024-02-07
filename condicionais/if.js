3d5850e079147560d29c78e83f892f46d0c7242c
Thalita coelho <thalita.bastos94@gmail.com>
2024-02-06 21:28:32 -0300

    condicionais if e else if

---
+++ condicionais/if.js
@@ -0,0 +1,11 @@
+const idade = 29;
+
+if (idade >= 18) {
+    // é executado caso a condição seja verdadeira
+    console.log("È maior de idade");
+} else {
+    // é executado caso a condição seja falsa
+    console.log("Não menor de idade")
+}
+
+console.log("fim do código");
